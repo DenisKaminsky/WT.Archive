8d924cde6a852df2caa6fe7984d047a2bd970b9d
DenisKaminsky <deniskaminsky123@mail.ru>
2018-12-03 22:17:20 +0300

    add book and user models

---
+++ src/by/bsuir/Kaminsky/models/Book.java
@@ -0,0 +1,67 @@
+package by.bsuir.Kaminsky.models;
+
+import java.io.Serializable;
+
+public class Book implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String author;
+	
+	private String title;
+	
+	private boolean isElectronic;
+	
+	public Book(String author,String title,boolean isElectronic){
+		this.author = author;
+		this.title = title;
+		this.isElectronic = isElectronic;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public void setAuthor(String value) {
+		author = value;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public void setTitle(String value) {
+		title = value;
+	}
+	
+	public boolean getIsElectronic(){
+		return isElectronic;
+	}
+		
+	public void setIsElectronic(Boolean value) {
+		isElectronic = value;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Book book;
+		
+		if (obj == this) 
+			return true;
+		if (obj == null || obj.getClass() != this.getClass())
+            return false;
+		book = (Book)obj;
+        return (isElectronic == book.isElectronic) && (title == book.title ||
+            (title != null && (title.toLowerCase()).equals(book.getTitle().toLowerCase())))
+                && (author == book.author || (author != null && (author.toLowerCase()).equals(book.getAuthor().toLowerCase())));
+	}
+	
+	@Override
+	public String toString() {
+		String type = (isElectronic)?"Electronic":"Paper";
+		return  "Title: " + title +
+                "\nAuthor: " + author +
+                "\nType: "+ type;
+	}
+}
+
---
+++ src/by/bsuir/Kaminsky/models/User.java
@@ -0,0 +1,66 @@
+package by.bsuir.Kaminsky.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String login;
+	
+	private String password;
+	
+	private boolean isAdministrator;
+	
+	public User(String login,String password,boolean isAdministrator){
+		this.login = login;
+		this.password = password;
+		this.isAdministrator = isAdministrator;
+	}
+	
+	public String getLogin(){
+		return login;
+	}
+	
+	public void setLogin(String value) {
+		login = value;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public void setPassword(String value) {
+		password = value;
+	}
+	
+	public boolean getIsAdministrator(){
+		return isAdministrator;
+	}
+	
+	public void setIsAdministrator(Boolean value) {
+		isAdministrator = value;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		User user;
+		
+		if (obj == this) 
+			return true;
+		if (obj == null || obj.getClass() != this.getClass())
+            return false;
+		user = (User)obj;
+        return ( login == user.login ||
+            ( login != null && login.equals(user.getLogin()) ) );
+	}
+	
+	@Override
+	public String toString() {
+		String type = (isAdministrator)?"Administrator":"User";
+		return  "Login: " + login +
+                "\nType: "+ type;
+	}
+	
+}
+
