1f2955d2375a50d52ab3de063092738bd699cb23
DenisKaminsky <deniskaminsky123@mail.ru>
2018-12-03 21:57:21 +0300

    add client view

---
+++ src/by/bsuir/Kaminsky/client/view/Main.java
@@ -0,0 +1,43 @@
+package by.bsuir.Kaminsky.client.view;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Main {
+	
+	private static final int PORT = 8080;
+	
+	public static void main(String[] args) throws IOException {
+        InetAddress addr = InetAddress.getByName("localhost");
+        ObjectOutputStream out;
+        ObjectInputStream in;
+        Controller controller = new Controller();
+
+        System.out.println(addr);
+        try (Socket socket = new Socket(addr, PORT)) {
+            System.out.println("SOCKET : " + socket);
+            out = new ObjectOutputStream (socket.getOutputStream());
+            in = new ObjectInputStream (socket.getInputStream());
+
+            while (true){
+                //ввод команды
+                String command = controller.waitCommand();
+                //отперавка команды на сервер
+                out.reset();
+                out.writeObject(controller.performCommand(command));
+                out.flush();
+                //вывод результата(ответ сервера)
+                XmlCollection result = (XmlCollection) in.readObject();
+                System.out.println(controller.getAnswer(result));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("<end> CLOSING SOCKET");
+        }
+
+    }
+}
